a2e349bff5c059f7bdbdccd0cd93b9fbbb4e6eaf
Nicola Zambello <nzambello@protonmail.com>
2021-05-18 18:57:38 +0200

    Preparing release 0.2.1

--- CHANGES.rst
+++ CHANGES.rst
@@ -2,7 +2,7 @@ Changelog
 =========
 
 
-0.2.1 (unreleased)
+0.2.1 (2021-05-18)
 ------------------
 
 - Add alt text on buttons
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='collective.z3cform.jsonwidget',
-    version='0.2.1.dev0',
+    version='0.2.1',
     description="Custom widget to manage complex json data stored into a text field",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
