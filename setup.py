928fa61725ab5627f7d1a07a1b78b8c4cd1661db
Andrea Cecchi <andrea.cecchi85@gmail.com>
2022-07-18 17:23:17 +0200

    Back to development: 1.1.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='collective.z3cform.jsonwidget',
-    version='1.1.0',
+    version='1.1.1.dev0',
     description="Custom widget to manage complex json data stored into a text field",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
